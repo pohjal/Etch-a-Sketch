5f696db0e32c6b1ea3f3896664d74559e9bd4b52
Leevi Pohja <124263697+pohjal@users.noreply.github.com>
2023-11-15 11:11:01 +0200

    grid koon vaihto onnistuu ja muut napit lisätty käyttöliittymään

--- scripts.js
+++ scripts.js
@@ -2,19 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
   maker();
 });
 
-let amount = 10;
+const button = document.querySelector("#myRange");
+const gridInfo = document.querySelector("#size");
+button.addEventListener("click", changeGrid);
+let amount = 16;
 
 function maker() {
   const cont = document.getElementById("oikea");
+  cont.innerHTML = "";
 
   for (let i = 0; i < amount * amount; i++) {
     const item = document.createElement("div");
 
-    const gridSize = 320 / amount;
+    const gridSize = 480 / amount;
     item.classList.add("grid-item");
 
     item.style.width = `${gridSize}px`;
     item.style.height = `${gridSize}px`;
     cont.appendChild(item);
   }
+}
+
+function changeGrid() {
+  gridInfo.textContent = button.value + " * " + button.value;
+  console.log(button.value);
+  amount = button.value;
+  maker();
 }
