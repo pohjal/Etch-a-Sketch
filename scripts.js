1a638d4a16b84b00b9da3d371b8ceac44d1f5d88
Leevi Pohja <124263697+pohjal@users.noreply.github.com>
2023-11-15 11:50:39 +0200

    eraser ja clear toimii

--- scripts.js
+++ scripts.js
@@ -4,11 +4,41 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const button = document.querySelector("#myRange");
 const gridInfo = document.querySelector("#size");
+const clear = document.querySelector("#clear");
+const eraser = document.querySelector("#eraser");
+
+eraser.addEventListener("click", eraserOnOff);
+clear.addEventListener("click", clearGrid);
 button.addEventListener("click", changeGrid);
 let amount = 16;
 
+let isMouseDown = false;
+let isEraserActive = false;
+
+const cont = document.getElementById("oikea");
+
+cont.addEventListener("mousedown", function () {
+  isMouseDown = true;
+});
+
+document.addEventListener("mouseup", function () {
+  isMouseDown = false;
+});
+
+// Add mousedown and mouseup event listeners to the container
+
+function eraserOnOff() {
+  isEraserActive = !isEraserActive;
+  if (isEraserActive == false) eraser.style.backgroundColor = "#e0e0e0";
+  if (isEraserActive == true) eraser.style.backgroundColor = "grey";
+}
+
+function clearGrid() {
+  cont.innerHTML = "";
+  maker();
+}
+
 function maker() {
-  const cont = document.getElementById("oikea");
   cont.innerHTML = "";
 
   for (let i = 0; i < amount * amount; i++) {
@@ -17,8 +47,27 @@ function maker() {
     const gridSize = 480 / amount;
     item.classList.add("grid-item");
 
+    item.addEventListener("click", function () {
+      // Change color on click
+      if (isEraserActive == false) item.style.backgroundColor = "black";
+      else {
+        item.style.backgroundColor = "white";
+      }
+    });
+
     item.style.width = `${gridSize}px`;
     item.style.height = `${gridSize}px`;
+
+    // Add mouseover event listener to each grid item
+    item.addEventListener("mouseover", function () {
+      if (isMouseDown) {
+        // Change color on mouseover while mouse button is pressed
+        if (isEraserActive == false) item.style.backgroundColor = "black";
+        else {
+          item.style.backgroundColor = "white";
+        }
+      }
+    });
     cont.appendChild(item);
   }
 }
